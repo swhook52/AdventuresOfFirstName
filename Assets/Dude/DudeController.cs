6de00d5eec575d8ab8513839cd3678b765547427
Steven Hook <steven@hookscode.com>
2015-08-03 20:35:56 -0400

    Progress from 7-30-15 hands on lab

---
+++ Assets/Dude/DudeController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class DudeController : MonoBehaviour
+{
+    public float RunSpeed;
+    public float JumpSpeed;
+    public Transform BulletPrefab;
+    public Transform BulletSpawn;
+
+    private Rigidbody2D _rigidBody;
+    private Animator _animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+
+        _rigidBody.AddForce(new Vector2(xMove * RunSpeed, 0));
+
+        if (Input.GetButton("Jump"))
+        {
+            _rigidBody.AddForce(new Vector2(0, JumpSpeed));
+            _animator.SetTrigger("Jump");
+        }
+    }
+}
